0a9896a021028967ffa9a94e8566bf03257c41f5
Misheck12 <livingi2018@gmail.com>
2022-11-02 23:28:38 +0200

    create home page

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -4,7 +4,18 @@ import "./Home.css";
 const Home = () => {
   return (
 	<>
-	<div className='Home'>home</div>
+	<div className='Home'>
+		<div className='Home-container'>
+			<div className='Home-header'>
+			<h1>
+				<q>Thousands have lived without love, not one without water.</q>H. Auden
+			</h1>
+			<p>Home is where the heart is.</p>
+			<button className='Home-header-btn'>Get Started</button>
+			</div>
+		</div>
+
+	</div>
 	</>
   )
 }
