33a1996d73a83255341b81454b0195c90369708d
Misheck12 <livingi2018@gmail.com>
2022-11-02 13:36:52 +0200

    crete and style navbar

---
+++ src/component/Navbar/MenuData.js
@@ -0,0 +1,38 @@
+export const MenuData = [ 
+    {
+    title: 'Home',
+    url: '#',
+    cName: 'nav-links',
+    icon : 'fa-solid fa-house-user'
+},
+{
+    title: 'About',
+    url: '#',
+    cName: 'nav-links',
+    icon : 'fa-solid fa-circle-info'
+},
+{
+    title: 'Services',
+    url: '#',
+    cName: 'nav-links',
+    icon : 'fa-solid fa-briefcase'
+},
+{
+    title: 'projects',
+    url: '#',
+    cName: 'nav-links',
+    icon : 'fa-solid fa-project-diagram'
+},
+{
+    title: 'Contact',
+    url: '#',
+    cName: 'nav-links',
+    icon : 'fa-solid fa-envelope'
+},
+{
+    title: 'Call-Us',
+    url: '#',
+    cName: 'nav-links',
+    icon : 'fa-solid fa-phone'
+},
+]
