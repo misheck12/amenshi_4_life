d89488b4cbb891189eac770627643819e1c95db0
Misheck Livingi <livingim2018@gmail.com>
2022-11-15 07:27:41 +0200

    create a gallery dummy page

---
+++ src/component/Gallery/Gallery.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Gallery.css'
+
+const Gallery = () => {
+  return (
+    <>
+        <div className="gallery">
+            <div className="gallery__item gallery__item--1">
+                <img src="https://images.unsplash.com/photo-1517436073-3b1b1b1b1b1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="episode 1" className="gallery__img" />
+            </div>
+            <div className="gallery__item gallery__item--2">
+                <img src="https://images.unsplash.com/photo-1517436073-3b1b1b1b1b1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="episode 2" className="gallery__img" />
+            </div>
+            <div className="gallery__item gallery__item--3">
+                <img src="https://images.unsplash.com/photo-1517436073-3b1b1b1b1b1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="episode 3" className="gallery__img" />
+            </div>
+            <div className="gallery__item gallery__item--4">
+                <img src="https://images.unsplash.com/photo-1517436073-3b1b1b1b1b1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="episode 4" className="gallery__img" />
+            </div>
+            <div className="gallery__item gallery__item--5">
+                <img src="https://images.unsplash.com/photo-1517436073-3b1b1b1b1b1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="episode 5" className="gallery__img" />
+            </div>
+            <div className="gallery__item gallery__item--6">
+                <img src="https://images.unsplash.com/photo-1517436073-3b1b1b1b1b1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="episode 6" className="gallery__img" />
+            </div>
+            </div>
+    </>
+  )
+}
+
+export default Gallery
