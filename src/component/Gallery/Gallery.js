3279537bf6012327beb9651bd073ee03d5a775c1
Misheck Livingi <livingim2018@gmail.com>
2022-11-19 18:39:52 +0200

    create gallery files

--- src/component/Gallery/Gallery.js
+++ src/component/Gallery/Gallery.js
@@ -4,26 +4,34 @@ import './Gallery.css'
 const Gallery = () => {
   return (
     <>
-        <div className="gallery">
-            <div className="gallery__item gallery__item--1">
-                <img src="https://images.unsplash.com/photo-1517436073-3b1b1b1b1b1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="episode 1" className="gallery__img" />
-            </div>
-            <div className="gallery__item gallery__item--2">
-                <img src="https://images.unsplash.com/photo-1517436073-3b1b1b1b1b1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="episode 2" className="gallery__img" />
-            </div>
-            <div className="gallery__item gallery__item--3">
-                <img src="https://images.unsplash.com/photo-1517436073-3b1b1b1b1b1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="episode 3" className="gallery__img" />
-            </div>
-            <div className="gallery__item gallery__item--4">
-                <img src="https://images.unsplash.com/photo-1517436073-3b1b1b1b1b1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="episode 4" className="gallery__img" />
-            </div>
-            <div className="gallery__item gallery__item--5">
-                <img src="https://images.unsplash.com/photo-1517436073-3b1b1b1b1b1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="episode 5" className="gallery__img" />
-            </div>
-            <div className="gallery__item gallery__item--6">
-                <img src="https://images.unsplash.com/photo-1517436073-3b1b1b1b1b1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="episode 6" className="gallery__img" />
-            </div>
-            </div>
+    <div className="gallery_container pt-40">
+    <div className="gallery">
+        <h1>Gallery</h1>
+        <p>Our showcase of works in pictures </p>
+    </div>
+        <div className="gallery__item mb-60">
+            <img src={require('./img/amenshi4.jpg')} alt="amenshi1" className="gallery__img " />
+            <img src={require('./img/amenshi8.jpg')} alt="amenshi2" className="gallery__img" />
+            <img src={require('./img/amenshi3.jpg')} alt="amenshi3" className="gallery__img" />
+            <img src={require('./img/amenshi16.jpg')} alt="amenshi4" className="gallery__img" />
+            <img src={require('./img/amenshi11.jpg')} alt="amenshi5" className="gallery__img" />
+            <img src={require('./img/amenshi18.jpg')} alt="amenshi6" className="gallery__img" />
+            <img src={require('./img/amenshi2.jpg')} alt="amenshi8" className="gallery__img" />
+            <img src={require('./img/amenshi9.jpg')} alt="amenshi9" className="gallery__img" />
+            <img src={require('./img/amenshi1.jpg')} alt="amenshi10" className="gallery__img" />
+            <img src={require('./img/amenshi5.jpg')} alt="amenshi11" className="gallery__img" />
+            <img src={require('./img/amenshi12.jpg')} alt="amenshi12" className="gallery__img" />
+            <img src={require('./img/amenshi13.jpg')} alt="amenshi13" className="gallery__img" />
+            <img src={require('./img/amenshi14.jpg')} alt="amenshi14" className="gallery__img" />
+            <img src={require('./img/amenshi10.jpg')} alt="amenshi15" className="gallery__img" />
+            <img src={require('./img/amenshi15.jpg')} alt="amenshi16" className="gallery__img" />
+            <img src={require('./img/amenshi17.jpg')} alt="amenshi17" className="gallery__img" />
+            <img src={require('./img/amenshi18.jpg')} alt="amenshi18" className="gallery__img" />
+            <img src={require('./img/amenshi19.jpg')} alt="amenshi19" className="gallery__img" />
+            <img src={require('./img/amenshi20.jpg')} alt="amenshi20" className="gallery__img" />
+            <img src={require('./img/amenshi21.jpg')} alt="amenshi21" className="gallery__img" />
+        </div>
+    </div>
     </>
   )
 }
