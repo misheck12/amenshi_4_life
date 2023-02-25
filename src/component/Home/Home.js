126cf8fe024dccd03da05af0cceb83635aef52fb
Misheck Livingi <livingim2018@gmail.com>
2023-02-25 15:02:29 +0200

    Add class names

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -27,7 +27,7 @@ const Home = () => {
 		</div>
     
     <div className='homes'>
-		<div className="flex flex-row ">
+		<div className="boxes flex-row ">
           <div className='w-1/2 h-12 md-10 pt-11'>
 			<img src={require('./img/back.jpg')} alt="home3" />
 		  </div>
@@ -63,7 +63,7 @@ const Home = () => {
             <p className="uppercase text-3xl text-green-600 font-bold mb-6 text-center">
               What we are doing now
             </p>
-            <p className="text-gray-500 mb-6 text-center m-20">
+            <p className="box text-gray-500 mb-20">
             Our borehole donation work involves drilling deep into the ground to access underground water sources,
             and installing pumps and other infrastructure to bring the water to the surface. This process can be 
             costly and time-consuming, but it is an effective way to provide clean, safe drinking water to communities
@@ -78,7 +78,7 @@ const Home = () => {
     <h2 className="text-3xl font-bold mb-12 text-center">Latest Projects</h2>
 
     <div className="grid lg:grid-cols-3 gap-6 xl:gap-x-12">
-      <div className="mb-6 lg:mb-0">
+      <div className=" project-section mb-6 lg:mb-0">
         <div>
           <div
             className="relative overflow-hidden bg-no-repeat bg-cover relative overflow-hidden bg-no-repeat bg-cover ripple shadow-lg rounded-lg mb-6"
@@ -94,7 +94,7 @@ const Home = () => {
           <div className="mb-3 text-red-600 font-medium text-sm flex items-center justify-center">
             <svg className="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d="M248 8C111.03 8 0 119.03 0 256s111.03 248 248 248 248-111.03 248-248S384.97 8 248 8zm82.29 357.6c-3.9 3.88-7.99 7.95-11.31 11.28-2.99 3-5.1 6.7-6.17 10.71-1.51 5.66-2.73 11.38-4.77 16.87l-17.39 46.85c-13.76 3-28 4.69-42.65 4.69v-27.38c1.69-12.62-7.64-36.26-22.63-51.25-6-6-9.37-14.14-9.37-22.63v-32.01c0-11.64-6.27-22.34-16.46-27.97-14.37-7.95-34.81-19.06-48.81-26.11-11.48-5.78-22.1-13.14-31.65-21.75l-.8-.72a114.792 114.792 0 0 1-18.06-20.74c-9.38-13.77-24.66-36.42-34.59-51.14 20.47-45.5 57.36-82.04 103.2-101.89l24.01 12.01C203.48 89.74 216 82.01 216 70.11v-11.3c7.99-1.29 16.12-2.11 24.39-2.42l28.3 28.3c6.25 6.25 6.25 16.38 0 22.63L264 112l-10.34 10.34c-3.12 3.12-3.12 8.19 0 11.31l4.69 4.69c3.12 3.12 3.12 8.19 0 11.31l-8 8a8.008 8.008 0 0 1-5.66 2.34h-8.99c-2.08 0-4.08.81-5.58 2.27l-9.92 9.65a8.008 8.008 0 0 0-1.58 9.31l15.59 31.19c2.66 5.32-1.21 11.58-7.15 11.58h-5.64c-1.93 0-3.79-.7-5.24-1.96l-9.28-8.06a16.017 16.017 0 0 0-15.55-3.1l-31.17 10.39a11.95 11.95 0 0 0-8.17 11.34c0 4.53 2.56 8.66 6.61 10.69l11.08 5.54c9.41 4.71 19.79 7.16 30.31 7.16s22.59 27.29 32 32h66.75c8.49 0 16.62 3.37 22.63 9.37l13.69 13.69a30.503 30.503 0 0 1 8.93 21.57 46.536 46.536 0 0 1-13.72 32.98zM417 274.25c-5.79-1.45-10.84-5-14.15-9.97l-17.98-26.97a23.97 23.97 0 0 1 0-26.62l19.59-29.38c2.32-3.47 5.5-6.29 9.24-8.15l12.98-6.49C440.2 193.59 448 223.87 448 256c0 8.67-.74 17.16-1.82 25.54L417 274.25z"/></svg>Travels
           </div>
-          <p className="text-gray-500 text-center">
+          <p className="text-gray-500">
             Kamatipa is home to 15,000 residents the area is populated by youths and children
             With few school and no clean water sources kamatipa suffers from water born diseases and low literacy.
             We have donated a borehole which is 30 meters deep to benefit the community , and we are hope that the story will change
@@ -102,8 +102,8 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="mb-6 lg:mb-0">
-        <div>
+      <div>
+        <div className='project-section'>
           <div
             className="relative overflow-hidden bg-no-repeat bg-cover relative overflow-hidden bg-no-repeat bg-cover ripple shadow-lg rounded-lg mb-6"
             data-mdb-ripple="true" data-mdb-ripple-color="light">
@@ -126,7 +126,7 @@ const Home = () => {
       </div>
 
       <div className="mb-0">
-        <div>
+        <div className='project-section'>
           <div
             className="relative overflow-hidden bg-no-repeat bg-cover relative overflow-hidden bg-no-repeat bg-cover ripple shadow-lg rounded-lg mb-6"
             data-mdb-ripple="true" data-mdb-ripple-color="light">
